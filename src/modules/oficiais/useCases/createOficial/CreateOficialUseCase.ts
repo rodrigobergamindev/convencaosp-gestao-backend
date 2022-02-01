d2517e91aadf53b4b5fe384c1417839e5cbafafc
rodrigobergamindev <rodrigobergamindev@gmail.com>
2022-02-01 19:22:30 -0300

    .

--- src/modules/oficiais/useCases/createOficial/CreateOficialUseCase.ts
+++ src/modules/oficiais/useCases/createOficial/CreateOficialUseCase.ts
@@ -12,7 +12,6 @@ class CreateOficialUseCase {
         const oficialAlreadyExist = this.oficiaisRepository.findByRO(ro)
 
         if(!oficialAlreadyExist){
-            console.log("Tá chegando");
             this.oficiaisRepository.create(oficial)
         }
         
